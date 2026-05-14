96c380f4355e6919375aa1d42d3680ae261d8420
José Vitor <vitor200450@gmail.com>
2026-05-14 01:07:57 -0300

    feat: implementar secao de solucoes com grade global

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
 import ProcessSection from "../componets/sections/processSection";
+import SolutionsSection from "../componets/sections/solutionsSection";
 
 export default function Home() {
   return (
-    <main className=""> 
+    <main className="bg-N1">
+      <div className="grid-page-bg mx-auto w-full max-w-[1920px]">
       <ProcessSection/>
+      <SolutionsSection/>
+      </div>
     </main>
   );
 }
