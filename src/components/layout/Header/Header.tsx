779d4cb80fdfe170dad62e1dd1bdbba10774e480
Alessandro Viana <alessandroviana830@gmail.com>
2026-05-15 20:35:12 -0300

    fix(Header): Corrigir a ordem das opções no header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -72,19 +72,19 @@ export const Header = () => {
                             className="flex justify-center items-center fixed top-0 left-0 backdrop-blur-[50px] w-dvw h-[55dvh] md:h-22.5 md:px-[12.5%] md:py-3.25 md:flex md:flex-row md:justify-end md: md:backdrop-blur-none"
                         >
                             <ul className="flex flex-col justify-start gap-2 w-[90%] font-zodiak font-bold text-N1 text-start text-[24px] md:flex-row md:gap-4 md:w-auto lg:gap-6 lg:text-[30px]">
-                                <li className="pl-4 pb-1 border-b border-N5 transition ease-in-out duration-300 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
+                                <li className="order-1 pl-4 pb-1 border-b border-N5 transition ease-in-out duration-300 md:order-1 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
                                     <Link href="/">Home</Link>
                                 </li>
-                                <li className="pl-4 pb-1 border-b border-N5 transition ease-in-out duration-300 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
+                                <li className="order-2 pl-4 pb-1 border-b border-N5 transition ease-in-out duration-300 md:order-2 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
                                     <Link href="/servicos">Serviços</Link>
                                 </li>
-                                <li className="pl-4 pb-1 border-b border-N5 transition ease-in-out duration-300 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
+                                <li className="order-3 pl-4 pb-1 border-b border-N5 transition ease-in-out duration-300 md:order-4 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
                                     <Link href="/cases">Cases</Link>
                                 </li>
-                                <li className="pl-4 pb-1 border-b border-N5 transition ease-in-out duration-300 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
+                                <li className="order-4 pl-4 pb-1 border-b border-N5 transition ease-in-out duration-300 md:order-5 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
                                     <Link href="/aboutus">Sobre nós</Link>
                                 </li>
-                                <li className="pl-4 pb-1 border-b border-N5 transition ease-in-out duration-300 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
+                                <li className="order-5 pl-4 pb-1 border-b border-N5 transition ease-in-out duration-300 md:order-3 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
                                     <Link href="/blog">Blog</Link>
                                 </li>
                             </ul>
