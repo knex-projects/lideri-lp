e145d863b759f832ba5aa12fe13da9360739a289
RoseneNunes <roseanesje2014@gmail.com>
2026-05-16 17:54:26 -0300

    feat: criar footer

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+
+export function Footer() {
+    return (
+        <section className="w-full flex flex-col min-h-[851px] sm:min-h-[551px] pb-[32px] px-[26px] pt-[100px] justify-between bg-R5 sm:px-[12.5%]">
+            <div className="mx-auto flex w-full flex-col ">
+                <div className="flex max-sm:flex-col flex-wrap   justify-between  ">
+                    <div className="flex items-center sm:justify-center max-[1161px]:w-full   py-8 sm:py-10">
+                        <Image
+                            src="/logo/lideriLogo.svg"
+                            alt="Lídere Jr. logo"
+                            width={237}
+                            height={91}
+                            className="h-auto w-auto"
+                        />
+                    </div>
+
+                    <div className="flex flex-col   sm:gap-8 pr-[26px] py-[40px]">
+                        <h3 className="text-[30px]  sm:text-[32px] font-zodiak max-sm:pb-[22px] font-semibold text-N3">Endereço</h3>
+                        <p className="text-[16px] sm:text-[20px] font-poppins leading-[1.6] text-N3 max-w-[244px]  sm:max-w-[207px]">
+                            UFPB - Campus I Centro de Ciências Sociais Aplicadas
+                            <br />
+                            Centro - João Pessoa, PB
+                        </p>
+                        <p className="text-[16px] sm:text-[20px] font-poppins leading-[1.6] text-N3"><strong className="font-bold">CNPJ:</strong> <br /> 19.521.379/0001-03</p>
+                    </div>
+                    <div className="grid max-2xl:grid-cols-1  grid-cols-2 gap-x-20">
+                        <div className="flex flex-col  pr-[26px] py-[40px]">
+                        <h3 className="text-[30px] pb-[22px] sm:text-[32px] font-zodiak font-semibold text-N3">Contatos</h3>
+                        <p className="text-[16px] sm:text-[20px] font-poppins leading-[1.6] text-N3">contato@liderejr.com.br</p>
+                        <p className="text-[16px] sm:text-[20px] font-poppins leading-[1.6] text-N3">(83) 99999-9999</p>
+                    </div>
+
+                    <div className="flex flex-col gap-[22px] sm:gap-8  pr-[26px] py-[40px]">
+                        <h3 className="text-[30px] sm:text-[32px] font-zodiak font-semibold text-N3">Redes sociais</h3>
+                        <div className="flex items-center gap-3 text-N3">
+                            <Image src="/assets/icon/instagram.svg" alt="Instagram" width={40} height={40} />
+                            <Image src="/assets/icon/linkedin.svg" alt="LinkedIn" width={40} height={40} />
+
+                        </div>
+
+                    </div>
+                </div>
+
+                    </div>
+                    
+
+            </div>
+            <p className="mt-10 text-start font-poppins text-[14px] sm:text-[16px] leading-[1.6] text-R2">
+                © 2026 Lídere Jr. Consultoria. Todos os direitos reservados.
+            </p>
+        </section>
+    );
+}
