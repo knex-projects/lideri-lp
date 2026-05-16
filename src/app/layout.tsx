ce3b7463d897b4c5b0db076fdc56e3a4f54ccebb
RoseneNunes <roseanesje2014@gmail.com>
2026-05-16 17:51:13 -0300

    feat: adicioanar footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat, Poppins } from "next/font/google";
 import "./globals.css";
 import { Header } from "../components/layout/Header/Header";
+import { Footer } from "../components/layout/footer";
 
 const zodiak = {
   variable: "--font-zodiak",
@@ -40,6 +41,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Header />
         {children}
+        <Footer/>
       </body>
     </html>
   );
