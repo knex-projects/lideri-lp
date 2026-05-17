f28100371ca2c941baeb62f87203f3bdc5ab26fc
Alessandro Viana <alessandroviana830@gmail.com>
2026-05-16 22:36:19 -0300

    Fix(Header): Alterar background e alinhamento do dropdown menu

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -5,23 +5,13 @@ import Link from "next/link"
 import { useEffect, useRef, useState } from "react"
 import { lideriIcon } from "@/public/assets"
 import { motion, AnimatePresence } from "motion/react"
-import { TextAlignJustify } from "lucide-react"
+import { Menu, TextAlignJustify, X } from "lucide-react"
 
 export const Header = () => {
     const [ isOpen, setIsOpen ] = useState(false)
     const [ isDesktop, setIsDesktop ] = useState(false)
     
     const menuRef = useRef<HTMLDivElement>(null)
-    
-    useEffect(() => {
-        function handleClickOutside(event: MouseEvent) {
-            if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-                setIsOpen(false)
-            }
-        }
-        
-        document.addEventListener("mousedown", handleClickOutside)
-    }, [menuRef])
 
     useEffect(() => {
         const handleResize = () => {
@@ -38,25 +28,61 @@ export const Header = () => {
         return () => window.removeEventListener("resize", handleResize)
     }, [])
 
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.overflow = "hidden"
+        } else {
+            document.body.style.overflow = ""
+        }
+    }, [isOpen])
+
     return (
-        <header className="fixed z-50 h-29 md:h-22.5">
-            <div className="flex justify-between items-center backdrop-blur-[10px] w-dvw h-full p-6.5 md:px-[12.5%] md:py-3.25">
+        <header className="absolute top-0 z-50 h-29 md:h-22.5">
+            <div className="flex justify-between items-center backdrop-blur-[50px] w-dvw h-full p-6.5 md:px-[12.5%] md:py-3.25">
                 <Image
                     src={ lideriIcon }
                     alt="Lideri logo"
+                    loading="eager"
                     width={ 64 }
                     height={ 64 }
                     className="text-R5"
                 />
                 
                 <button
-                    className="flex justify-center items-center gap-3 w-27.5 h-10.75 px-3 py-2 rounded-lg border border-N1 font-poppins text-N1 text-lg font-normal md:hidden"
+                    className="flex justify-center items-center w-27.5 h-10.75 px-3 py-2 rounded-lg border border-N1 font-poppins text-N1 text-lg font-normal md:hidden"
                     onClick={ () => {
                         setIsOpen(!isOpen)
                     } }
                 >
-                    Menu
-                    <TextAlignJustify size={ 24 } strokeWidth={ 3 } className="text-R3"/>
+                    <AnimatePresence mode="sync">
+                        {
+                            isOpen ? (
+                                <motion.span
+                                    key="exit"
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    exit={{ opacity: 0 }}
+                                    transition={{ duration: 0.4, ease: "easeOut" }}
+                                    className="flex justify-center items-center absolute"
+                                >
+                                    Fechar
+                                    <X size={ 24 } strokeWidth={ 3 } className="text-R3"/>
+                                </motion.span>
+                            ) : (
+                                <motion.span
+                                    key="menu"
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    exit={{ opacity: 0 }}
+                                    transition={{ duration: 0.4, ease: "easeOut" }}
+                                    className="flex justify-center items-center absolute gap-3"
+                                >
+                                    Menu
+                                    <TextAlignJustify size={ 24 } strokeWidth={ 3 } className="text-R3"/>
+                                </motion.span>
+                            )
+                        }
+                    </AnimatePresence>
                 </button>
             </div>
             
@@ -69,22 +95,22 @@ export const Header = () => {
                             exit={{ opacity: 0 }}
                             transition={{ duration: 0.4 }}
                             ref={ menuRef }
-                            className="flex justify-center items-center fixed top-0 left-0 backdrop-blur-[50px] w-dvw h-[55dvh] md:h-22.5 md:px-[12.5%] md:py-3.25 md:flex md:flex-row md:justify-end md: md:backdrop-blur-none"
+                            className="flex justify-center absolute top-29 left-0 bg-B9/55 backdrop-blur-[50px] bg- w-dvw h-dvh md:top-0 md:h-22.5 md:px-[12.5%] md:py-3.25 md:flex md:flex-row md:justify-end md:bg-[unset] md:backdrop-blur-none"
                         >
-                            <ul className="flex flex-col justify-start gap-2 w-[90%] font-zodiak font-bold text-N1 text-start text-[24px] md:flex-row md:gap-4 md:w-auto lg:gap-6 lg:text-[30px]">
-                                <li className="order-1 pl-4 pb-1 border-b border-N5 transition ease-in-out duration-300 md:order-1 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
+                            <ul className="flex flex-col gap-2 w-[90%] font-zodiak font-bold text-N1 text-[24px] md:flex-row md:gap-4 md:w-auto lg:gap-6 lg:text-[30px]">
+                                <li className="order-1 pl-4 pb-1 border-b border-N5 text-center transition ease-in-out duration-300 md:order-1 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
                                     <Link href="/">Home</Link>
                                 </li>
-                                <li className="order-2 pl-4 pb-1 border-b border-N5 transition ease-in-out duration-300 md:order-2 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
+                                <li className="order-2 pl-4 pb-1 border-b border-N5 text-center transition ease-in-out duration-300 md:order-2 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
                                     <Link href="/servicos">Serviços</Link>
                                 </li>
-                                <li className="order-3 pl-4 pb-1 border-b border-N5 transition ease-in-out duration-300 md:order-4 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
+                                <li className="order-3 pl-4 pb-1 border-b border-N5 text-center transition ease-in-out duration-300 md:order-4 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
                                     <Link href="/cases">Cases</Link>
                                 </li>
-                                <li className="order-4 pl-4 pb-1 border-b border-N5 transition ease-in-out duration-300 md:order-5 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
+                                <li className="order-4 pl-4 pb-1 border-b border-N5 text-center transition ease-in-out duration-300 md:order-5 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
                                     <Link href="/aboutus">Sobre nós</Link>
                                 </li>
-                                <li className="order-5 pl-4 pb-1 border-b border-N5 transition ease-in-out duration-300 md:order-3 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
+                                <li className="order-5 pl-4 pb-1 border-b border-N5 text-center transition ease-in-out duration-300 md:order-3 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
                                     <Link href="/blog">Blog</Link>
                                 </li>
                             </ul>
