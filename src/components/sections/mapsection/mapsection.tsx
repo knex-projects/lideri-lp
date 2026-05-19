c44dcf1befcf4342aefd7d35ee57c30136616a41
RoseneNunes <roseanesje2014@gmail.com>
2026-05-19 14:47:23 -0300

    style: centralizar textos

--- src/components/sections/mapsection/mapsection.tsx
+++ src/components/sections/mapsection/mapsection.tsx
@@ -4,11 +4,11 @@ import InteractiveWorldMap from './InteractiveWorldMap';
 
 export default function MapWrapper() {
   return (
-    <div className=" w-full sm:h-[1014px] min-h-[860px] flex flex-col justify-baseline  sm:justify-center sm:items-center py-[40px] sm:py-20 px-[12.5%] max-sm:px-[26px]">
+    <div className=" w-full sm:h-[1014px] min-h-[860px] flex flex-col justify-baseline text-center sm:justify-center sm:items-center py-[40px] sm:py-20 px-[12.5%] max-sm:px-[26px]">
 
-      <h1 className='sm:text-[48px] pt-[30px] sm:pt-[40px] text-[36px] max-w-[361px] font-zodiac font-normal leading-14 text-N8 w-fit sm:max-w-[553px] leading-snug '>Países prospectados em <strong className='text-R5 font-normal'>mapa.</strong></h1>
-      <p className='sm:text-[16px] text-[14px] w-fit pb-[40px] max-w-[361px]  sm:max-w-[553px] text-N5 font-poppins '>Veja a seguir os países em que já trabalhamos representados em mapa.</p>
-      <div className='w-full sm:h-[576px] h-[500px]'>
+      <h1 className='sm:text-[48px]  text-[36px] max-w-[361px] font-zodiac font-normal leading-14 text-N8 w-fit sm:max-w-[553px] leading-snug '>Países prospectados em <strong className='text-R5 font-normal'>mapa.</strong></h1>
+      <p className='sm:text-[16px] text-[14px] w-fit pb-[81px] max-w-[361px]  sm:max-w-[553px] text-N5 font-poppins '>Veja a seguir os países em que já trabalhamos representados em mapa.</p>
+      <div className='w-full  sm:h-[576px] h-[500px]'>
         <InteractiveWorldMap/>
       </div>
 
