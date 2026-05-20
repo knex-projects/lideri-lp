d756b6cff5f3f4a123a84b3a14021d1b1260fc2d
RoseneNunes <roseanesje2014@gmail.com>
2026-05-19 23:55:26 -0300

    style: ajustar cor do menu , e tamanhos dos links e da logo

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -39,14 +39,16 @@ export const Header = () => {
     return (
         <header className="absolute top-0 z-50 h-29 md:h-22.5 w-full">
             <div className="flex justify-between items-center backdrop-blur-[50px] w-full h-full p-6.5 md:px-[12.5%] md:py-3.25">
-                <Image
+                <div  className="relative w-[144px] h-[55px] md:w-[186px] md:h-[72px]">
+                    <Image
                     src={lideriIcon}
                     alt="Lideri logo"
                     loading="eager"
-                    width={186}
-                    height={72}
-                    className="text-R5"
+                    fill
+                   
                 />
+                </div>
+                
 
                 <button
                     className="flex justify-center items-center w-27.5 h-10.75 px-3 py-2 rounded-lg border border-N1 font-montserrat text-N1 text-lg font-normal md:hidden"
@@ -66,19 +68,19 @@ export const Header = () => {
                                     className="flex justify-center items-center absolute"
                                 >
                                     Fechar
-                                    <X size={24} strokeWidth={3} className="text-R3" />
+                                    <X size={24} strokeWidth={3} className="text-N4" />
                                 </motion.span>
                             ) : (
                                 <motion.span
                                     key="menu"
-                                    initial={{ opacity: 0 }}
+                                    initial={{ opacity: 0  }}
                                     animate={{ opacity: 1 }}
                                     exit={{ opacity: 0 }}
-                                    transition={{ duration: 0.4, ease: "easeOut" }}
+                                    transition={{ duration: 0.6, ease: "easeOut" }}
                                     className="flex justify-center items-center absolute gap-3"
                                 >
                                     Menu
-                                    <TextAlignJustify size={24} strokeWidth={3} className="text-R3" />
+                                    <TextAlignJustify size={24} strokeWidth={3} className="text-N4" />
                                 </motion.span>
                             )
                         }
@@ -95,22 +97,22 @@ export const Header = () => {
                             exit={{ opacity: 0 }}
                             transition={{ duration: 0.4 }}
                             ref={menuRef}
-                            className="flex justify-center absolute top-29 left-0 bg-B9/55 backdrop-blur-[50px] bg- w-full h-dvh md:top-0 md:h-22.5 md:px-[12.5%] md:py-3.25 md:flex md:flex-row md:justify-end md:bg-R1/5 md:backdrop-blur-none"
+                            className="flex justify-center absolute top-29 left-0 bg-B9/55 backdrop-blur-[50px]  w-full h-dvh md:top-0 md:h-22.5 md:px-[12.5%] md:py-3.25 md:flex md:flex-row md:justify-end md:bg-R1/5 md:backdrop-blur-none"
                         >
-                            <ul className="flex flex-col items-center gap-2 w-[90%] font-montserrat font-normal text-N1 text-[20px] md:flex-row md:gap-4 md:w-auto lg:gap-10 ">
-                                <li className="order-1 pl-4  border-b border-N5 text-center transition ease-in-out duration-300 md:order-1 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
+                            <ul className="flex flex-col max-md:pt-[128px] items-center gap-2 w-[90%] font-montserrat font-normal text-N1 text-[20px] md:flex-row md:gap-4 md:w-auto lg:gap-10 ">
+                                <li className="order-1 pl-4 max-md:w-full  border-b border-N5 text-center transition ease-in-out duration-300 md:order-1 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
                                     <Link href="/#heroSection">Home</Link>
                                 </li>
-                                <li className="order-2 pl-4 pb-1 border-b border-N5 text-center transition ease-in-out duration-300 md:order-2 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
+                                <li className="order-2 pl-4 max-md:w-full pb-1 border-b border-N5 text-center transition ease-in-out duration-300 md:order-2 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
                                     <Link href="/#processo">Serviços</Link>
                                 </li>
-                                <li className="order-3 pl-4 pb-1 border-b border-N5 text-center transition ease-in-out duration-300 md:order-4 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
+                                <li className="order-3 pl-4 max-md:w-full pb-1 border-b border-N5 text-center transition ease-in-out duration-300 md:order-4 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
                                     <Link href="/#projetos">Cases</Link>
                                 </li>
-                                <li className="order-4 hidden pl-4 pb-1 border-b border-N5 text-center transition ease-in-out duration-300 md:order-5 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
+                                <li className="order-4 hidden max-md:w-full pl-4 pb-1 border-b border-N5 text-center transition ease-in-out duration-300 md:order-5 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
                                     <Link href="/#aboutus">Sobre nós</Link>
                                 </li>
-                                <li className="order-5 pl-4 pb-1 border-b border-N5 hidden text-center transition ease-in-out duration-300 md:order-3 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
+                                <li className="order-5 pl-4 pb-1 max-md:w-full border-b border-N5 hidden text-center transition ease-in-out duration-300 md:order-3 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
                                     <Link href="/blog">Blog</Link>
                                 </li>
                             </ul>
