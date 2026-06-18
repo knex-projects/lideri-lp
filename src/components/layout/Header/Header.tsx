ed99ab2735d1f6fa557b41dfebb7633c4cfdc755
Alessandro Viana <alessandroviana830@gmail.com>
2026-06-18 16:34:18 -0300

    Fix(Header): Adicionar botão de Contato e espaçamento no menu mobile

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -43,7 +43,7 @@ export const Header = () => {
 
     return (
         <header className="absolute top-0 z-50 h-29 md:h-22.5 w-full">
-            <div className={ `flex justify-between items-center w-full h-full p-6.5 md:py-3.25 lg:px-[12.5%]  ${ isGlass ? "backdrop-blur-[50px]" : "bg-[#0D1122]" }` }>
+            <div className={ `flex justify-between items-center w-full h-full p-6.5 md:py-3.25 xl:px-[12.5%]  ${ isGlass ? "backdrop-blur-[50px]" : "bg-[#0D1122]" }` }>
                 <div  className="relative w-36 h-13.75 md:w-46.5 md:h-18">
                     <Image
                     src={lideriIcon}
@@ -102,23 +102,62 @@ export const Header = () => {
                             exit={{ opacity: 0 }}
                             transition={{ duration: 0.4 }}
                             ref={menuRef}
-                            className="flex justify-center absolute top-29 left-0 bg-B9/55 backdrop-blur-[50px] w-full h-dvh md:top-0 md:h-22.5 md:px-6.5 md:py-3.25 md:flex md:flex-row md:justify-end md:bg-R1/5 md:backdrop-blur-none lg:px-[12.5%]"
+                            className="flex justify-center absolute top-29 left-0 bg-B9/55 backdrop-blur-[50px] w-full h-dvh md:top-0 md:h-22.5 md:px-6.5 md:py-3.25 md:flex md:flex-row md:justify-end md:bg-R1/5 md:backdrop-blur-none xl:px-[12.5%]"
                         >
-                            <ul className="flex flex-col max-md:pt-32 items-center gap-2 w-[90%] font-montserrat font-normal text-N1 text-[20px] md:flex-row md:gap-4 md:w-auto lg:gap-10 ">
-                                <li className="order-1 max-md:w-full  border-b border-N5 text-center transition ease-in-out duration-300 md:order-1 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
-                                    <Link onClick={()=>{setIsOpen(false)}} href="/#heroSection">Home</Link>
+                            <ul className="flex flex-col max-md:pt-32 items-center w-[90%] font-montserrat font-normal text-N1 text-lg md:text-xl md:flex-row md:gap-4 md:w-auto lg:gap-10 ">
+                                <li className="order-1 w-full border-y border-N5 md:border-none md:order-1">
+                                    <Link
+                                        onClick={()=>{setIsOpen(false)}}
+                                        href="/#heroSection"
+                                        className="block w-full max-md:w-full py-4 text-center text-nowrap transition ease-in-out duration-300 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1"
+                                    >
+                                        Home
+                                    </Link>
                                 </li>
-                                <li className="order-2 max-md:w-full pb-1 border-b border-N5 text-center transition ease-in-out duration-300 md:order-2 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
-                                    <Link onClick={()=>{setIsOpen(false)}}  href="/#processo">Serviços</Link>
+                                <li className="order-3 w-full border-b border-N5 md:border-none md:order-2">
+                                    <Link 
+                                        onClick={()=>{setIsOpen(false)}}
+                                        href="/#processo"
+                                        className="block w-full max-md:w-full py-4 text-center text-nowrap transition ease-in-out duration-300 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1"
+                                    >
+                                        Serviços
+                                    </Link>
                                 </li>
-                                <li className="order-3  max-md:w-full pb-1 border-b border-N5 text-center transition ease-in-out duration-300 md:order-4 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
-                                    <Link onClick={()=>{setIsOpen(false)}}  href="/#projetos">Cases</Link>
+                                <li className="order-4 w-full border-b border-N5 md:border-none md:order-4">
+                                    <Link 
+                                        onClick={()=>{setIsOpen(false)}}
+                                        href="/#projetos"
+                                        className="block w-full max-md:w-full py-4 text-center text-nowrap transition ease-in-out duration-300 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1"
+                                    >
+                                        Cases
+                                    </Link>
                                 </li>
-                                <li className="order-4 max-md:w-full  pb-1 border-b border-N5 text-center transition ease-in-out duration-300 md:order-5 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
-                                    <Link onClick={()=>{setIsOpen(false)}}  href="/#aboutus">Sobre nós</Link>
+                                <li className="order-2 w-full border-b border-N5 md:border-none md:order-5">
+                                    <Link 
+                                        onClick={()=>{setIsOpen(false)}}
+                                        href="/#aboutus"
+                                        className="block w-full max-md:w-full  py-4 text-center text-nowrap transition ease-in-out duration-300 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1"
+                                    >
+                                        Sobre nós
+                                    </Link>
                                 </li>
-                                <li className="order-5  pb-1 max-md:w-full border-b border-N5 text-center transition ease-in-out duration-300 md:order-3 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1">
-                                    <Link onClick={()=>{setIsOpen(false)}}  href="/blog">Blog</Link>
+                                <li className="order-5 w-full border-b border-N5 md:border-none md:order-3">
+                                    <Link 
+                                        onClick={()=>{setIsOpen(false)}}
+                                        href="/blog"
+                                        className="block w-full py-4 max-md:w-full text-center text-nowrap transition ease-in-out duration-300 md:p-0 md:border-transparent md:hover:border-b-2 md:hover:border-N1"
+                                    >
+                                        Blog
+                                    </Link>
+                                </li>
+                                <li className="order-6 w-full md:order-6">
+                                    <Link
+                                        onClick={()=>{setIsOpen(false)}}
+                                        href="/Contato"
+                                        className="block w-full bg-[#87240E] py-4 text-center text-nowrap font-montserrat font-bold md:font-normal md:px-6 md:py-3 md:rounded-lg"
+                                    >
+                                        Contato
+                                    </Link>
                                 </li>
                             </ul>
                         </motion.nav>
