61e2703c45d852ac8af38a3c7555fcfbcd03725c
Alessandro Viana <alessandroviana830@gmail.com>
2026-06-18 19:50:10 -0300

    Refactor(Footer): Adicionar ícones e alterar a estilização de alguns elementos

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -74,15 +74,46 @@ export function Footer() {
                                     <Image src="/assets/icon/linkedin.svg" alt="LinkedIn" width={40} height={40} />
                                 </a>
                             </div>
-
+                            <div className="flex gap-5">
+                                <Clock3 className="shrink-0"/>
+                                <p className="leading-[1.6]">
+                                    Segunda a Sexta
+                                    08h às 18h
+                                </p>
+                            </div>
                         </div>
                     </div>
 
+                    <div className="flex flex-col gap-8 2xl:justify-self-end">
+                        <div className="flex flex-col gap-3">
+                            <h3 className="text-[30px] sm:text-[32px] font-zodiak font-normal text-N3">Redes sociais</h3>
+                            <div className="w-12 h-1 rounded-[10px] bg-[#87240E]"></div>
+                        </div>
+
+                        <p className="font-montserrat sm:text-lg text-N3">Acompanhe nossas redes e fique por dentro das novidades da Líderi.</p>
+
+                        <div className="flex items-center gap-6 text-N3">
+                            <a
+                                href="https://www.instagram.com/liderijr"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="border border-[#C1C2BE] p-2.25 rounded-lg"
+                            >
+                                <Image src="/assets/icon/instagram.svg" alt="Instagram" width={34} height={34} />
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/in/lidericonsultoria"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="border border-[#C1C2BE] p-2.25 rounded-lg"
+                            >
+                                <Image src="/assets/icon/linkedin.svg" alt="LinkedIn" width={34} height={34} />
+                            </a>
+                        </div>
+                    </div>
                 </div>
-
-
             </div>
-            <p className="mt-10 text-start font-montserrat text-[14px] sm:text-[16px] leading-[1.6] text-R2">
+            <p className="text-center font-montserrat text-[14px] leading-[1.6] text-N3 md:text-start">
                 © 2026 Líderi Jr. Consultoria. Todos os direitos reservados.
             </p>
         </section>
