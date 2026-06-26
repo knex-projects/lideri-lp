83a2b0435545e75141dcd655ca1756a2e338bda8
Carlos Franco <kadusa09@gmail.com>
2026-06-26 15:44:37 -0300

    feat: add section Nosso Blog

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { AboutUs } from "./sections/aboutsection/AboutUs"
 export { default as ProcessSection } from './sections/processSection'
 export { default as SolutionsSection } from './sections/solutionsSection'
 export { default as PartnersSection } from './sections/partnersSection'
+export { BlogSection } from './sections/BlogSection'
