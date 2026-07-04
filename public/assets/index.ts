626b80c7af1de56ac5d5e804ffd9c5f4e0588160
RoseneNunes <roseanesje2014@gmail.com>
2026-07-04 16:53:45 -0300

    feat: adicionar rota do bg da tela de login

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -10,6 +10,7 @@ export const lacemakerImg = "/assets/images/lacemaker.png";
 export const equipe1 = "/assets/images/equipeDaLideri.svg";
 export const equipe2 = "/assets/images/equipeDaLideri2.svg";
 export const chama1 = "/assets/images/chama.svg";
+export const loginBg = "/assets/images/loginBg.svg";
 export const chama2 = "/assets/images/chama2.svg";
 export const logoLideri1 = "/logo/lideriLogo.svg";
 export const logoLideri2 = "/logo/lideriLogo2.svg";
@@ -21,7 +22,6 @@ export const appBadging = "/assets/icon/app_badging.svg";
 export const arrowRightBlog = "/assets/icon/arrow-right-blog.svg";
 export const arrowUpRightBlog = "/assets/icon/arrow-up-right-blog.svg";
 export const arrowUpRightBlogMobile = "/assets/icon/arrow-up-right-blog-mobile.svg";
-
 export const callLog = "/assets/icon/callLog.svg";
 export const chat = "/assets/icon/chat.svg";
 export const exploreNearby = "/assets/icon/exploreNearby.svg";
