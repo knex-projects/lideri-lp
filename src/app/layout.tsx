48e494180bbbe8b9f8e27bb9088bc0d80fd3c936
RoseneNunes <roseanesje2014@gmail.com>
2026-07-21 15:39:18 -0300

    feat: adicionar proteção de rotas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { Header } from "../components/layout/Header/Header";
 import { Footer } from "../components/layout/footer";
+import { Providers } from "./provider";
 
 const montserrat = Montserrat({
   weight: ["400", "500", "600", "700"],
@@ -30,7 +31,7 @@ export default function RootLayout({
       className={`${montserrat.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
