f88b1d63ba6ec372b5f7fc1a5216a915ec0597d2
RoseneNunes <roseanesje2014@gmail.com>
2026-08-12 13:44:01 -0300

    fix: configurar email.js pra envio das informaçoes

--- src/components/sections/contactsection/ContactSection.tsx
+++ src/components/sections/contactsection/ContactSection.tsx
@@ -7,6 +7,7 @@ import { useRef, useState } from "react"
 import { RiInstagramFill } from "react-icons/ri"
 import { LuLinkedin } from "react-icons/lu"
 import emailjs from "@emailjs/browser"
+import toast from "react-hot-toast"
 
 export const ContactSection = () => {
     const form = useRef<HTMLFormElement>(null)
@@ -16,29 +17,58 @@ export const ContactSection = () => {
     const sendEmail = (e: React.SyntheticEvent) => {
         e.preventDefault()
 
-        emailjs.sendForm("serviceId", "templateId", form.current!, "publicKey").then(
-            () => {
-                form.current!.reset()
-                setTelefone(undefined)
-            },
-            (error) => {
-                alert(`Falha ao enviar a mensagem. Tente novamente, ${ error.text }`)
-            }
-        )
+        const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID
+        const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID
+        const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+
+        if (!serviceId || !templateId || !publicKey) {
+            toast.error("Erro nas configurações de envio de e-mail.")
+            return
+        }
+
+       
+        const loadingToast = toast.loading("Enviando sua mensagem...")
+
+        emailjs
+            .sendForm(serviceId, templateId, form.current!, { publicKey: publicKey })
+            .then(
+                () => {
+                   
+                    toast.dismiss(loadingToast)
+                    toast.success("Mensagem enviada com sucesso!")
+                    form.current!.reset()
+                    setTelefone(undefined)
+                },
+                (error) => {
+                   
+                    toast.dismiss(loadingToast)
+                    toast.error(`Falha ao enviar a mensagem: ${error.text}`)
+                }
+            )
     }
 
     return (
         <section className="pb-25 pt-45 px-6.5 overflow-x-hidden text-N8 md:py-60 md:px-[12.5%]">
             <div>
-                <h1 className="mb-4 font-[impact] text-[2.25rem] text-center md:text-5xl">Para onde podemos te levar?</h1>
-                <p className="font-Montserrat text-sm text-center md:text-[1.75rem]">Vamos entender e encontrar o melhor plano para escalar seu negócio</p>
-                
+                <h1 className="mb-4 font-[impact] text-[2.25rem] text-center md:text-5xl">
+                    Para onde podemos te levar?
+                </h1>
+                <p className="font-Montserrat text-sm text-center md:text-[1.75rem]">
+                    Vamos entender e encontrar o melhor plano para escalar seu negócio
+                </p>
+
                 <div className="flex flex-col md:items-center md:mt-15 xl:flex-row xl:gap-30 3xl:gap-[15vw]">
                     <div className="mt-10 mb-15 bg-N3 rounded-lg p-4 w-full max-w-180 sm:mx-0 md:px-8 md:py-12 xl:mb-0 xl:mt-0">
-                        <form ref={ form } onSubmit={ sendEmail } className="flex flex-col gap-6 text-N8">
+                        <form
+                            ref={form}
+                            onSubmit={sendEmail}
+                            className="flex flex-col gap-6 text-N8"
+                        >
                             <div className="grid grid-cols-2 grid-rows-1 gap-x-3">
                                 <div className="flex flex-col gap-2">
-                                    <label htmlFor="nome" className="font-bold md:text-2xl">Nome</label>
+                                    <label htmlFor="nome" className="font-bold md:text-2xl">
+                                        Nome
+                                    </label>
                                     <input
                                         type="text"
                                         name="nome"
@@ -50,7 +80,9 @@ export const ContactSection = () => {
                                 </div>
 
                                 <div className="flex flex-col gap-2">
-                                    <label htmlFor="sobrenome" className="font-bold md:text-2xl">Sobrenome</label>
+                                    <label htmlFor="sobrenome" className="font-bold md:text-2xl">
+                                        Sobrenome
+                                    </label>
                                     <input
                                         type="text"
                                         id="sobrenome"
@@ -63,7 +95,9 @@ export const ContactSection = () => {
                             </div>
 
                             <div className="flex flex-col gap-2">
-                                <label htmlFor="email" className="font-bold md:text-2xl">Email</label>
+                                <label htmlFor="email" className="font-bold md:text-2xl">
+                                    Email
+                                </label>
                                 <input
                                     type="email"
                                     name="email"
@@ -75,17 +109,21 @@ export const ContactSection = () => {
                             </div>
 
                             <div className="flex flex-col gap-2">
-                                <label htmlFor="telefone" className="font-bold md:text-2xl">Número</label>
-                                <Input value={ telefone } onChange={ setTelefone } />
-                                <input type="hidden" name="telefone" value={ telefone || "" }/>
+                                <label htmlFor="telefone" className="font-bold md:text-2xl">
+                                    Número
+                                </label>
+                                <Input value={telefone} onChange={setTelefone} />
+                                <input type="hidden" name="telefone" value={telefone || ""} />
                             </div>
-                            
+
                             <div className="flex flex-col gap-2">
-                                <label htmlFor="mensagem" className="font-bold md:text-2xl">Mensagens</label>
+                                <label htmlFor="mensagem" className="font-bold md:text-2xl">
+                                    Mensagens
+                                </label>
                                 <textarea
                                     name="mensagem"
                                     id="mensagem"
-                                    rows={ 4 }
+                                    rows={4}
                                     placeholder="Descreva seus interesses"
                                     required
                                     className="px-6 py-4 border-[0.15625rem] border-N6 rounded-lg bg-N1 placeholder:font-Montserrat placeholder:text-sm placeholder:text-N4 md:text-lg transition-colors ease-in-out focus:border-R5 focus:outline-none md:placeholder:text-lg"
@@ -158,53 +196,72 @@ export const ContactSection = () => {
                                 </div>
                             </div>
 
-                            <button type="submit" className="cursor-pointer py-3 rounded-lg bg-R5 font-Montserrat text-lg font-bold text-N1 transition-all ease-in-out duration-300 hover:bg-R4">Enviar</button>
+                            <button
+                                type="submit"
+                                className="cursor-pointer py-3 rounded-lg bg-R5 font-Montserrat text-lg font-bold text-N1 transition-all ease-in-out duration-300 hover:bg-R4"
+                            >
+                                Enviar
+                            </button>
                         </form>
                     </div>
 
                     <div className="flex flex-col gap-5 md:justify-center md:gap-18">
                         <div className="flex items-start gap-5">
                             <Image
-                                src={ chat }
-                                width={ 46 }
-                                height={ 46 }
+                                src={chat}
+                                width={46}
+                                height={46}
                                 alt="Ícone de chat"
                                 className="md:w-[62px]"
                             />
                             <div className="flex flex-col justify-center items-start gap-2">
-                                <h2 className="mb-1 font-[impact] text-[2rem]">Converse com nós</h2>
-                                <p className="font-Montserrat text-sm text-N5 md:text-lg">Nossa equipe está ativa para ajudar</p>
-                                <p className="font-Montserrat text-sm text-N5 md:text-lg"><em>contato@lidericonsultoria.com</em></p>
+                                <h2 className="mb-1 font-[impact] text-[2rem]">
+                                    Converse com nós
+                                </h2>
+                                <p className="font-Montserrat text-sm text-N5 md:text-lg">
+                                    Nossa equipe está ativa para ajudar
+                                </p>
+                                <p className="font-Montserrat text-sm text-N5 md:text-lg">
+                                    <em>contato@lidericonsultoria.com</em>
+                                </p>
                             </div>
                         </div>
 
                         <div className="flex items-start gap-5">
                             <Image
-                                src={ exploreNearby }
-                                width={ 46 }
-                                height={ 46 }
+                                src={exploreNearby}
+                                width={46}
+                                height={46}
                                 alt="Ícone de explore nearby"
                                 className="md:w-[62px]"
                             />
                             <div className="flex flex-col justify-center items-start gap-2">
                                 <h2 className="mb-1 font-[impact] text-[2rem]">Visite-nos</h2>
-                                <p className="font-Montserrat text-sm text-N5 md:text-lg">Venha conhecer nossa sede</p>
-                                <p className="font-Montserrat text-sm text-N5 md:text-lg"><em>rua, nº, bairro, João Pessoa-PB; 58050-725</em></p>
+                                <p className="font-Montserrat text-sm text-N5 md:text-lg">
+                                    Venha conhecer nossa sede
+                                </p>
+                                <p className="font-Montserrat text-sm text-N5 md:text-lg">
+                                    <em>rua, nº, bairro, João Pessoa-PB; 58050-725</em>
+                                </p>
                             </div>
                         </div>
 
                         <div className="flex items-start gap-5">
                             <Image
-                                src={ callLog }
-                                width={ 46 }
-                                height={ 46 }
+                                src={callLog}
+                                width={46}
+                                height={46}
                                 alt="Ícone de call log"
                                 className="md:w-[62px]"
                             />
                             <div className="flex flex-col justify-center items-start gap-2">
                                 <h2 className="mb-1 font-[impact] text-[2rem]">Contate-nos</h2>
-                                <p className="font-Montserrat text-sm text-N5 md:text-lg">De Segunda à Sexta, das 8h às 17h</p>
-                                <p className="font-Montserrat text-sm text-N5 md:text-lg"><em>+55 (83) 9125-5249</em></p>
+                                <p className="font-Montserrat text-sm text-N5 md:text-lg">
+                                    De Segunda à Sexta, das 8h às 17h
+                                </p>
+                                <p className="font-Montserrat text-sm text-N5 md:text-lg">
+                                    <em>+55 (83) 9125-5249</em>
+                                </p>
                             </div>
                         </div>
 
@@ -215,7 +272,7 @@ export const ContactSection = () => {
                                 rel="noopener noreferrer"
                                 className="flex justify-center items-center w-15 h-15 rounded-[1.1875rem] text-R5 transition-all duration-500 hover:bg-R5 hover:text-N1"
                             >
-                                <RiInstagramFill size={ 60 } className="scale-110" />
+                                <RiInstagramFill size={60} className="scale-110" />
                             </a>
                             <a
                                 href="https://www.linkedin.com/in/lidericonsultoria"
@@ -223,7 +280,7 @@ export const ContactSection = () => {
                                 rel="noopener noreferrer"
                                 className="flex justify-center items-center w-15 h-15 bg-R5 rounded-2xl text-N1 border-3 transition-all duration-500 hover:bg-N1 hover:border-R5 hover:text-R5"
                             >
-                                <LuLinkedin size={ 33.33 } className="scale-115"/>
+                                <LuLinkedin size={33.33} className="scale-115" />
                             </a>
                         </div>
                     </div>
